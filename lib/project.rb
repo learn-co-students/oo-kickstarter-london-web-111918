7b832401fe234747dd72542eb59451c01bb28a6a
Sunny <sunny2831@hotmail.co.uk>
2018-11-29 14:05:54 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+class Project
+
+  attr_accessor :title, :backers, :backer_name
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer_name)
+    @backers << backer_name
+    backer_name.backed_projects << self    
+  end
+end
+
+
+project = Project.new("some title")
